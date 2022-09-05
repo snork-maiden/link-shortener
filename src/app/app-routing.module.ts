05112127a4217afc77625138bac61de66ce511de
snork-maiden <pollykoldysheva@gmail.com>
2022-09-05 15:40:45 +0300

    Add routing, base layout, base styes, favicon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: AuthorizationComponent,
+      }
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseLayoutComponent,
+    AuthorizationComponent
   ],
   imports: [
     BrowserModule,
