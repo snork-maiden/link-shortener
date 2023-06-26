5b78ea50ff46acac50c9559344c844729dc80b38
snork-maiden <snork-maiden@users.noreply.github.com>
2023-06-26 14:25:25 +0000

    deploy: 75cf0e137dc80c6b5783b1a923931a3bb2655b3a

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM node:18-alpine
-WORKDIR /opt/app
-COPY ./ /opt/app
-RUN npm install -g @angular/cli && npm install
--- src/app/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  APIDomain: 'https://s.offout.am/',
-  APIDomainShort: 's.offout.am/s',
-};
